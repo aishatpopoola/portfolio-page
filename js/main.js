66466ceeb27376c557694cf5f7e44dff3f47da46
aishatpopoola <aishatpopoola906@gmail.com>
2022-05-18 15:00:02 +0100

    feat:i design the page for mobile screen

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// /*==================== MENU SHOW Y HIDDEN ====================*/
+// const navMenu = document.querySelector('#nav_menu'),
+//     navToggle = document.querySelector('#nav_toggle'),
+//     navClose = document.querySelector('#nav_close')
+
+
+// /*===== MENU SHOW =====*/
+// /* Validate if constant exists */
+
+// const navToggler = () => {
+//     navMenu.classList.add('show-menu')
+// }
+// if (navToggle) {
+//     navToggle.addEventListener('click', navToggler)
+// }
+
+
+// /*===== MENU HIDDEN =====*/
+// /* Validate if constant exists */
+// const navCloser = () => {
+//     navMenu.classList.remove('show-menu')
+// }
+// if (navClose) {
+//     navClose.addEventListener('click', navCloser)
+// }
